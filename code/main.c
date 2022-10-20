802260be9b2a09762c1dc56841a55ce8d51a6c1b
Baumel Márton <marcibaumel@gmail.com>
2022-10-20 16:05:03 +0200

    almost done

--- code/main.c
+++ code/main.c
@@ -2,105 +2,103 @@
 #include <stdlib.h>
 #include <immintrin.h>
 
-#define HEADER_SIZE 54
-#define FILE1_SIZE 1080000
-#define FILE2_SIZE 4320000
-
-void setHeaderTo600x600(unsigned char *);
-
 int main(int argc, char const *argv[])
 {
+    //Need variables 
+    int headerSize = 112;
+    int fileSize1 = 1080000;
+    int fileSize2 = 4320000;
+    char header[headerSize];
+    int i;
+    int j;
 
-  // Set variables
-  int i;
-  unsigned char *header = (unsigned char *)malloc(HEADER_SIZE * sizeof(unsigned char));
-  unsigned char *space1 = (unsigned char *)malloc(FILE1_SIZE * sizeof(unsigned char));
-  unsigned char *space2 = (unsigned char *)malloc((FILE2_SIZE / 2) * sizeof(unsigned char));
-  unsigned char *space2Body = (unsigned char *)malloc(FILE1_SIZE * sizeof(unsigned char));
-  unsigned char *newImage = (unsigned char *)malloc(FILE1_SIZE * sizeof(unsigned char));
 
-  FILE *space_1_file = fopen("space_1.bmp", "rb");
-  FILE *space_2_file = fopen("space_2.bmp", "rb");
-  FILE *new_space_file = fopen("space_new.bmp", "wb");
-  
-  // Existing files
-  if (space_2_file == NULL || space_1_file == NULL || new_space_file == NULL)
-  {
-    printf("---\tERROR\t---\n");
-    exit(1);
-  }
+    //Allocated the memory space
+    char *file1 = (char *)malloc(fileSize1 * sizeof(char));
+    char *file2= (char *)malloc((fileSize2) * sizeof(char));
+    char *mfSpace2 = (char *)malloc(fileSize1 * sizeof(char));
+    char *newBody = (char *)malloc(fileSize1 * sizeof(char));
 
-  // Reading and set headers
-  fread(header, sizeof(unsigned char), HEADER_SIZE, space_1_file);
-  fseek(space_2_file, HEADER_SIZE, SEEK_SET);
-  setHeaderTo600x600(header);
+    FILE *space_1 = fopen("space_1.bmp", "rb");
+    FILE *space_2 = fopen("space_2.bmp", "rb");
+    FILE *new_space_file = fopen("space_merged.bmp", "wb");
 
-  // Reading space1 data body
-  fread(space1, sizeof(unsigned char), FILE1_SIZE, space_1_file);
-  i = 0;
-
-  // Reading space2 data body
-  for (i = 0; i < FILE2_SIZE; i += (1200 * 3))
-  {
-    fread(&space2[i], sizeof(unsigned char), 1200 * 3, space_2_file);
-    if (fseek(space_2_file, 1200 * 3, SEEK_CUR) != 0)
+    //Check the files
+    if (space_2 == NULL || space_1 == NULL || new_space_file == NULL)
     {
-      break;
-    };
-  }
+        printf("FILE ERROR");
+        return 0;
+    }
 
-  // Calculating space2 data body
-  int j;
-  for (i = 0, j = 0; j < FILE1_SIZE; i += 6, j += 3)
-  {
-    space2Body[j] = (space2[i] + space2[i + 3600]) / 2;
-    //space2Body[j + 1] = (space2[i + 1] + space2[i + 4]) / 2;
-    //space2Body[j + 2] = (space2[i + 2] + space2[i + 5]) / 2;
-  }
+    // Reading and set headers from file1
+    fread(header, sizeof(char), headerSize, space_1);
+    fseek(space_2, headerSize, SEEK_SET);
 
-  // Calculating new space data body
-  for (i = 0; i < FILE1_SIZE; i += 32)
-  {
-    __m256i mm_a = _mm256_loadu_si256((__m256i *)&(space1[i]));
-    __m256i mm_b = _mm256_loadu_si256((__m256i *)&(space2Body[i]));
+    // Read the file1 and file2 picture data
+    fread(file1, sizeof(char), fileSize1, space_1);
+    fread(file2, sizeof(char), fileSize2, space_2);
 
-    __m256i mm_c = _mm256_avg_epu8(mm_a, mm_b);
+    // Recalculate the second picture body
+    char *file2Column = (char *)malloc(fileSize1 * sizeof(char));
+    char *file2Row = (char *)malloc(fileSize1 * sizeof(char));
 
-    _mm256_storeu_si256((__m256i *)&(newImage[i]), mm_c);
-  }
+    // Check the color value
+    for (i = 0, j = 0; j < fileSize1 - 3600; i += 6, j += 3)
+    {
+        //See the column
+        file2Column[j] = file2[i];
 
-  // Writing header data
-  fwrite(header, sizeof(unsigned char), sizeof(unsigned char) * HEADER_SIZE, new_space_file);
+        //See the row element value
+        file2Row[j] = file2[i + 3600];
 
-  // Writing body data
-  fwrite(newImage, sizeof(unsigned char), sizeof(unsigned char) * FILE1_SIZE, new_space_file);
+        //The second value
+        file2Column[j + 1] = file2[i + 1];
+        file2Row[j + 1] = file2[i + 3601];
 
-  // Free allocated memory space
-  free(header);
-  free(newImage);
-  free(space1);
-  free(space2);
-  free(space2Body);
-  fclose(space_1_file);
-  fclose(space_2_file);
-  fclose(new_space_file);
-  return 0;
-}
+        //The third value
+        file2Column[j + 2] = file2[i + 2];
+        file2Row[j + 2] = file2[i + 3602];
 
-void setHeaderTo600x600(unsigned char *header)
-{
-  header[2] = 0xF6;
-  header[3] = 0x7A;
-  header[4] = 0x10;
-  header[18] = 88;
-  header[19] = 2;
-  header[22] = 88;
-  header[23] = 2;
-  header[34] = 0xC0;
-  header[35] = 0x7A;
-  header[36] = 0x10;
-  header[38] = 196;
-  header[39] = 14;
-  header[42] = 196;
-  header[43] = 14;
+        //Check end of the row
+        if ((i % 3600) == 0)
+        {
+            i += 3600;
+        }
+    }
+
+    // Merging the body for the new image
+    for (i = 0; i < fileSize1; i += 32)
+    {
+        //Moves integer values from unaligned memory location to a destination vector
+        __m256i columnData = _mm256_loadu_si256((__m256i *)&file2Column[i]);
+        __m256i rowData = _mm256_loadu_si256((__m256i *)&file2Row[i]);
+
+        //Computes the average of unsigned 8/16-bit integer data elements of two vectors. 
+        __m256i avgData = _mm256_avg_epu8(columnData, rowData);
+
+        //Moves integer values from unaligned memory location to a destination vector
+        __m256i requiredSize = _mm256_loadu_si256((__m256i *)&(file1[i]));
+
+        //Computes the average of unsigned 8/16-bit integer data elements of two vectors. 
+        __m256i newImage = _mm256_avg_epu8(requiredSize, avgData);
+
+        //Moves values from a integer vector to an unaligned memory location.
+        _mm256_storeu_si256((__m256i *)&(newBody[i]), newImage);
+    }
+
+    // Make the header for the merged image
+    fwrite(header, sizeof(char), sizeof(char) * headerSize, new_space_file);
+
+    // Make the picture for the merged image
+    fwrite(newBody, sizeof(char), sizeof(char) * fileSize1, new_space_file);
+
+    // Liberate the allocated elements
+    free(newBody);
+    free(file1);
+    free(file2);
+    free(mfSpace2);
+    fclose(space_1);
+    fclose(space_2);
+    fclose(new_space_file);
+    return 0;
 }
